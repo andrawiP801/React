4e5a07846ea425d98aa2a98fece8035a90a42f5b
GrupoOperativo <tdagrupooperativo@gmail.com>
2023-11-14 21:08:08 -0600

    third

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
-import './Sidebar.css';
+import React from "react";
+import "./Sidebar.css";
+import homeIcon from "./home.png";
+import configIcon from "./config.png";
+import perfilIcon from "./perfil.png";
+import principalIcon from "./principal.png";
+import ranksIcon from "./ranks.png";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-header">
-        <h2>App Name</h2>
+        <h1>Corluss</h1>
       </div>
       <ul className="sidebar-menu">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li>
+          <a href="#home">
+            <img src={homeIcon} alt="Home" /> Principal
+          </a>
+        </li>
+        <li>
+          <a href="#menu">
+            <img src={principalIcon} alt="Menu" /> Menú
+          </a>
+        </li>
+        <li>
+          <a href="#profile">
+            <img src={perfilIcon} alt="Profile" /> Perfil
+          </a>
+        </li>
+        <li>
+          <a href="#ranking">
+            <img src={ranksIcon} alt="Ranking" /> Rankings
+          </a>
+        </li>
+        <li>
+          <a href="#settings">
+            <img src={configIcon} alt="Settings" /> Configuración
+          </a>
+        </li>
       </ul>
-      <div className="sidebar-footer">
-        <p>User Info</p>
-      </div>
     </div>
   );
 };
